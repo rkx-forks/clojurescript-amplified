a879d8584a8c8b4ca15260b6f04c1c6e7ea653e1
Norman Kabir <nkabir@gmail.com>
2022-03-31 07:58:48 -0500

    adding mdx support -- need to add components

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,29 @@
-const appConfig = require('../webpack.config.js');
-const webpack = require('webpack');
+const appConfig = require("../webpack.config.js");
+const webpack = require("webpack");
 
 module.exports = {
-  "stories": [
-    "../public/js/stories/*_stories.js"
+  stories: [
+    "../public/js/stories/*_stories.js",
+    "../public/js/stories/*.stories.mdx",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
   ],
-  "core": {
-    "builder": "webpack5"
+  core: {
+    builder: "webpack5",
   },
   webpackFinal: (config) => {
     const { rules } = config.module;
     config.module.rules = [...rules, ...appConfig.module.rules];
 
-    config.plugins.push(new webpack.ProvidePlugin({
-      process: 'process/browser',
-    }));
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        process: "process/browser",
+      })
+    );
 
     return config;
   },
-}
+};
